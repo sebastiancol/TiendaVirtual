37dddda447f612a3940c69435840786c7b8c3b29
sebastian <sebastian1475@hotmail.com>
2024-01-15 21:09:54 -0500

    se implementan helpers al proyecto

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -1,5 +1,6 @@
 <?php
 
+use models\DataBase;
 class Register extends Controllers{
 
     public function __construct()
@@ -7,16 +8,21 @@ class Register extends Controllers{
         parent::__construct();
     }
 
-    public function main($paramns)
+    public function main(
+        //$paramns
+    )
     {
         /*$data['data_page']="home"; 
         $data['data_title']="pagina principal"; 
         $data['data_name']="home"; */
-        $this->views->getView($this,"registrar"); 
+        $this->views->getView($this,"registrar",$data); 
         // require_once('./Views/login.php');
     }
 
-          
+    public function insertar(){
+
+        $data = $this->model->setUser();
+    }      
    
 
     
